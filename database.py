047e006a68e4f6e471efd0b2d248378c5ded4e40
beatrizmoreira190 <beatrizmoreira190@gmail.com>
2025-11-10 11:36:04 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_PUBLIC_URL")
 
 # ⚙️ Correção automática do prefixo
 # O Render usa 'postgres://' mas o SQLAlchemy precisa de 'postgresql://'
